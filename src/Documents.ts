5045bc8770390dfc1e8d0b0a2894103bf6cf1ae2
Magnus Welander <magnus.welander@riksarkivet.se>
2015-12-16 14:43:54 +0100

    Moved SupportsFullscreen() to Documents class

--- src/Documents.ts
+++ src/Documents.ts
@@ -8,5 +8,14 @@ module Utils{
                 return true;
             }
         }
+
+        static SupportsFullscreen(): boolean {
+            var doc = document.documentElement;
+            var support = doc.requestFullscreen || doc.mozRequestFullScreen ||
+                        doc.webkitRequestFullScreen || doc.msRequestFullscreen;
+            
+            return support != undefined;
+            
+        }
     }
 }
