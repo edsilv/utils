6570ee92dc2a4e1acde3ef903741fecbbd8be689
edsilv <e.silverton@gmail.com>
2017-12-04 18:44:18 +0000

    delete typings.json

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,7 @@ const tasks = require('gulp-tasks');
 
 tasks.init({
     metadata: metadata,
-    // libs that MUST be included in a consuming app for this component to work
     libs: [
         'node_modules/extensions/dist/extensions.js'
-    ],
-    // libs that MAY be included in a consuming app but are used here for example purposes
-    examples: []
+    ]
 });
