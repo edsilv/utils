3fcedd66c599399996509ccf9e4ce07616cd1ab0
edsilv <e.silverton@gmail.com>
2018-03-02 15:08:55 +0000

    bumper minor version

--- dist/utils.d.ts
+++ dist/utils.d.ts
@@ -1,4 +1,4 @@
-// utils v0.1.2 https://github.com/edsilv/utils
+// utils v0.2.0 https://github.com/edsilv/utils
 declare function escape(s: string): any;
 declare function unescape(s: string): any;
 interface CanvasRenderingContext2D {
